99482d0e4993251d3b849787c49068741c5baaac
Allan Vital <allan.vital@gmail.com>
2016-12-18 18:18:56 -0200

    updating view

--- goal/views.py
+++ goal/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
 from .models import Goal, Entry
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    latest_goal = Goal.objects.order_by('-year')[:1] # there must be an obviously better way
-    template = loader.get_template('goal/index.html')
+    latest_goals = Goal.objects.order_by('-year')[:1] # there must be an obviously better way
+    if len(latest_goals) > 0:
+        latest_goal = latest_goals[0]
+    else:
+        latest_goal = None
     context = {
-        'goal' : latest_goal[0]
+        'goal' : latest_goal
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'goal/index.html', context)
