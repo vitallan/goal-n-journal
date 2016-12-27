6f8bcbbe9f7119387770c8c110193285f3f2a583
Allan Vital <allan.vital@gmail.com>
2016-12-27 21:33:38 -0200

    better layout!

--- goal/views.py
+++ goal/views.py
@@ -7,27 +7,33 @@ from django.utils import timezone
 def index(request):
     latest_goals = Goal.objects.order_by('-year')[:1] # there must be an obviously better way
     if len(latest_goals) > 0:
-        return render(request, 'goal/index.html', {'goal' : latest_goals[0]})
+        goal = latest_goals[0]
+        entries = Entry.objects.filter(goal=goal)
+        return render(request, 'goal/index.html', {'goal' : goal, "entries" : entries })
     return render(request, 'goal/new-goal.html')
 
 def log(request, goal_id):
     log = request.POST['log']
-    hours_worked = request.POST['hours_worked']
-    print(goal_id + " - " + log + " - " + hours_worked);
+    hours_worked = request.POST['hours-worked']
     goal = get_object_or_404(Goal, pk=goal_id)
-    entry = Entry()
-    entry.entry_date = timezone.now()
-    entry.goal = goal
-    entry.description = log
+    entry = Entry(
+            entry_date = timezone.now(),
+            goal = goal,
+            description = log,
+            hours_worked = hours_worked
+        )
     entry.save()
     return HttpResponseRedirect(reverse('goal:index'))
 
 def new_goal(request):
-    goal_name = request.POST['goal_name']
-    goal_year = request.POST['goal_year']
-    goal = Goal()
-    goal.name = goal_name
-    goal.year = goal_year
+    if request.method == "GET":
+        return render(request, 'goal/new-goal.html', { 'simple_new' : True })
+    goal_name = request.POST['goal-name']
+    goal_year = request.POST['goal-year']
+    goal = Goal(
+            name = goal_name,
+            year = goal_year
+        )
     goal.save()
     return HttpResponseRedirect(reverse('goal:index'))
 
