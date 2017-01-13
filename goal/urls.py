5ad557b9d8308b226c624412c048da0e62bbd3a1
Allan Vital <allan.vital@gmail.com>
2017-01-13 20:55:03 -0200

    home screen added

--- goal/urls.py
+++ goal/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'goal'
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.home, name="home"),
+    url(r'^logout/$', views.logout, name="logout"),
+    url(r'^today/$', views.index, name="index"),
     url(r'^(?P<goal_id>[0-9]+)/log/$', views.log, name="log"),
     url(r'^new/$', views.new_goal, name="new_goal"),
 ]
--- goal/views.py
+++ goal/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url="/?unauthorized=True")
 def index(request):
-    latest_goals = Goal.objects.filter(user_id=request.user.id).order_by('-year')[:1]  # there must be an obviously better way
+    latest_goals = Goal.objects.filter(user_id=request.user.id).order_by('-year')[:1]  # there must be an obviously
+    # better way
     if len(latest_goals) > 0:
         goal = latest_goals[0]
         entries = Entry.objects.filter(goal=goal)
@@ -13,6 +18,7 @@ def index(request):
     return render(request, 'goal/new-goal.html')
 
 
+@login_required(login_url="/?unauthorized=True")
 def log(request, goal_id):
     log = request.POST['log']
     hours_worked = request.POST['hours-worked']
@@ -27,6 +33,7 @@ def log(request, goal_id):
     return HttpResponseRedirect(reverse('goal:index'))
 
 
+@login_required(login_url="/?unauthorized=True")
 def new_goal(request):
     if request.method == "GET":
         return render(request, 'goal/new-goal.html', {'simple_new': True})
@@ -39,3 +46,12 @@ def new_goal(request):
     )
     goal.save()
     return HttpResponseRedirect(reverse('goal:index'))
+
+
+def home(request):
+    return render(request, 'goal/home.html')
+
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('goal:home'))
