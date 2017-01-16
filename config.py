09540c4cc334d88ae49cd06e06944f00df143319
Allan Vital <allan.vital@gmail.com>
2017-01-16 19:42:36 -0200

    wiring it up together and new favicon

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'iw0ka-6rt68=^ytq66@ub$y9fjk5v7u-e6u=wttyh5ju$&7w8q'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # url: /login/twitter
 
 SOCIAL_AUTH_TWITTER_KEY = 'secret'
