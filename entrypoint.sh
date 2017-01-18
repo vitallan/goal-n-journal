7dc0d64633b7586cb539d1d1bbce89b9a7330a0b
Allan Vital <allan.vital@gmail.com>
2017-01-18 09:36:31 -0200

    entrypoint polishing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 mv /goalnjournal/live_conf/config.py /goalnjournal/config.py
 python /goalnjournal/manage.py makemigrations
 python /goalnjournal/manage.py migrate
-cp -r /goalnjournal/goal/static /goalnjournal/static
+cp -r /goalnjournal/goal/static/* /goalnjournal/static
 gunicorn --bind 0.0.0.0:8000 goalnjournal.wsgi:application
