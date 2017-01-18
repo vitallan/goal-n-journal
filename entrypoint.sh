1c2f431b0b893cc11bbf55902f64fcf52f839b71
Allan Vital <allan.vital@gmail.com>
2017-01-17 23:22:44 -0200

    updating entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mv live_conf/config.py .
-python manage.py makemigrations
-python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 goalnjournal.wsgi:application
+mv /goalnjournal/live_conf/config.py /goalnjournal/config.py
+python /goalnjournal/manage.py makemigrations
+python /goalnjournal/manage.py migrate
+gunicorn --bind 0.0.0.0:8000 /goalnjournal/goalnjournal.wsgi:application
